7ca1a0b061e042d46101997e2035306e9201c630
Mr.ture.China <mr.true.china@gmail.com>
2022-01-03 13:23:00 +0800

    格式化

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 // 添加消息发出的监听器
 chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    // 不知道为什么这里面 console 会失效，同时如果使用 alert 会因为不能即时反应报错
+    function (request, sender, sendResponse) {
+        // 不知道为什么这里面 console 会失效，同时如果使用 alert 会因为不能即时反应报错
 
-    // 如果发出的消息是下载图片（"type" 也是自定义的，发出消息的参数是一个对象，里面的字段可以自由定义）
-    if (request.type === "downloadImage"){
-      downloadImage(request.fileName, request.url);
+        // 如果发出的消息是下载图片（"type" 也是自定义的，发出消息的参数是一个对象，里面的字段可以自由定义）
+        if (request.type === "downloadImage") {
+            downloadImage(request.fileName, request.url);
+        }
     }
-  }
 );
 
 /**
@@ -15,10 +15,9 @@ chrome.runtime.onMessage.addListener(
  * @param {string} fileName 下载后的文件名，包括后缀名
  * @param {string} url 下载链接
  */
-function downloadImage(fileName, url){
-  console.log(chrome);
-  chrome.downloads.download({
-    filename: fileName,
-    url: url
-  });
+function downloadImage(fileName, url) {
+    chrome.downloads.download({
+        filename: fileName,
+        url: url
+    });
 }
