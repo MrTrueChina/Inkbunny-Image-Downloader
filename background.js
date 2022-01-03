212559e38ae8496d9c4a8be9133e4fc06532af53
Mr.ture.China <mr.true.china@gmail.com>
2022-01-03 13:14:51 +0800

    移除 background 中不需要的颜色代码

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
-
 // 添加消息发出的监听器
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
