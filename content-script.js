74f63ff1300af1e6f9b0ab28d02153395c58a015
Mr.ture.China <mr.true.china@gmail.com>
2022-01-03 15:55:46 +0800

    添加获取作者名称、作者ID、图片名称的方法

--- content-script.js
+++ content-script.js
@@ -42,7 +42,7 @@ function addDownloadButton() {
 function downloadButtonClick() {
     if (isSingleImage()) {
         // 当前图片是单图，直接下载当前网页上的单图
-        downloadSingleImage(document);
+        downloadSingleImage();
     } else {
         // 否则使用多图方式下载
         downloadMultipleImage();
@@ -60,9 +60,20 @@ function isSingleImage() {
 
 /**
  * 下载单张图片
- * @param {Document} inkbunnyDoc Inkbunny 主站的图片浏览网页的 Document 对象
  */
-function downloadSingleImage(inkbunnyDoc) {
+function downloadSingleImage(){
+
+    getOriginImageName();
+
+    // downloadSingleImageByDocument(document,"page.jpg");
+}
+
+/**
+ * 通过 Document 下载单张图片
+ * @param {Document} inkbunnyDoc Inkbunny 主站的图片浏览网页的 Document 对象
+ * @param {string} fileName 下载后的文件名，包括后缀名
+ */
+function downloadSingleImageByDocument(inkbunnyDoc, fileName) {
     // 获取到正在浏览的图片的元素
     let imageElement = inkbunnyDoc.getElementById("magicbox");
     // 向上获取到 同时包含图片和下载按钮的层级的 div
@@ -72,10 +83,8 @@ function downloadSingleImage(inkbunnyDoc) {
     let aList = imageAndDownloadDiv.getElementsByTagName("a");
     let downloadA = aList[aList.length - 1];
 
-    let testName = "page.jpg";
-
     // 向 background 发出下载图片消息，downloads 在注入内容里不能使用，需要向 background 发消息让 background 使用
-    chrome.runtime.sendMessage({ type: "downloadImage", fileName: testName, url: downloadA.href });
+    chrome.runtime.sendMessage({ type: "downloadImage", fileName: fileName, url: downloadA.href });
 }
 
 /**
@@ -117,10 +126,110 @@ function downloadImageByHttpGet(url){
             let doc = new DOMParser().parseFromString(responseText, "text/html");
 
             // 下载这个网页的主图
-            downloadSingleImage(doc);
+            downloadSingleImageByDocument(doc,"page.jpg");
         }
     };
     
     // 发送请求
     httpRequest.send();
 }
+
+/**
+ * 获取单张图片下载后的名字
+ */
+function getSingleImageName(){
+
+}
+
+/**
+ * 获取图片作者名字
+ */
+function getAuthorName(){
+
+    // 获取图片作者名字元素包装元素时的根元素的 id
+    let getAuthorNameContainerElementRootId = "pictop";
+    // 获取图片作者名字元素包装元素时从根元素开始到获取到图片名称元素包装元素所需的所有标签和索引
+    let getAuthorNameContainerElementTagPath = [["table", 0], ["tbody", 0], ["tr", 0], ["td", 1], ["div", 0], ["table", 0], ["tbody", 0], ["tr", 0], ["td", 0], ["a", 1]];
+    // 图片作者名字元素
+    let authorNameElement;
+
+    // 根据 id 获取根元素
+    authorNameElement = document.getElementById(getAuthorNameContainerElementRootId);
+
+    // 遍历标签找到图片名称元素
+    getAuthorNameContainerElementTagPath.forEach(tag => {
+        authorNameElement = authorNameElement.getElementsByTagName(tag[0])[tag[1]];
+    });
+
+    // 获取作者名称文本
+    let authorName = authorNameElement.textContent;
+
+    // 去除前后空格
+    authorName = authorName.replace(/(^\s*)|(\s*$)/g, "");
+
+    // 去除作者名后面的 “的作品” 和作品数量
+    authorName = authorName.substring(0, authorName.lastIndexOf("'s Gallery"));
+
+    return authorName;
+}
+
+/**
+ * 获取图片作者 ID
+ */
+function getAuthorId() {
+    // 获取图片作者头像元素包装元素时的根元素的 id
+    let getAuthorHeadContainerElementRootId = "pictop";
+    // 获取图片作者头像元素包装元素时从根元素开始到获取到图片名称元素包装元素所需的所有标签和索引
+    let getAuthorHeadContainerElementTagPath = [["table", 0], ["tbody", 0], ["tr", 0], ["td", 1], ["div", 0], ["table", 0], ["tbody", 0], ["tr", 0], ["td", 0], ["a", 0]];
+    // 图片作者头像元素
+    let authorHeadElement;
+
+    // 根据 id 获取根元素
+    authorHeadElement = document.getElementById(getAuthorHeadContainerElementRootId);
+
+    // 遍历标签找到图片名称元素
+    getAuthorHeadContainerElementTagPath.forEach(tag => {
+        authorHeadElement = authorHeadElement.getElementsByTagName(tag[0])[tag[1]];
+    });
+
+    // 获取作者的链接，这个链接可以提取出 ID
+    let authorId = authorHeadElement.href;
+
+    // 去除作者个人空间网址的固定部分
+    authorId = authorId.replace("https://inkbunny.net/", "");
+
+    return authorId;
+}
+
+/**
+ * 获取图片原名
+ */
+function getOriginImageName() {
+    // 获取图片名称元素包装元素时的根元素的 id
+    let getImageNameContainerElementRootId = "pictop";
+    // 获取图片名称元素包装元素时从根元素开始到获取到图片名称元素包装元素所需的所有标签和索引
+    let getImageNameContainerElementTagPath = [["table", 0], ["tbody", 0], ["tr", 0], ["td", 1], ["div", 0], ["table", 0], ["tbody", 0], ["tr", 2], ["td", 0], ["h1", 0]];
+    // 图片名称元素
+    let imageNameElement;
+
+    // 根据 id 获取根元素
+    imageNameElement = document.getElementById(getImageNameContainerElementRootId);
+
+    // 遍历标签找到图片名称元素
+    getImageNameContainerElementTagPath.forEach(tag => {
+        imageNameElement = imageNameElement.getElementsByTagName(tag[0])[tag[1]];
+    });
+
+    // 获取图片名称
+    let imageName = imageNameElement.textContent;
+
+    // 去除前后空格
+    authorName = authorName.replace(/(^\s*)|(\s*$)/g, "");
+
+    return imageName;
+}
+
+/**
+ * 获取图片 ID
+ */
+function getImageId(){}
